6087365f2c9cc8f5b7c733ba94af883ae1be79d7
Bartosz Gałek <bartosz.galek@allegro.pl>
2021-01-22 11:27:46 +0100

    Update tracking.jsx

--- components/tracking.jsx
+++ components/tracking.jsx
@@ -14,6 +14,7 @@ const Tracking = () => {
                       function gtag(){dataLayer.push(arguments);}
                       gtag('js', new Date());
                       gtag('config', 'G-6H60V3S51H');
+                `
                 }}
             />
         </div>
