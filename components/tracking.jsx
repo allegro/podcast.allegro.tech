8eaf4fa566b593b10c498020aa8830e7cdb465b9
Bartosz Gałek <bartosz.galek@allegro.pl>
2021-01-22 11:22:21 +0100

    Update tracking.jsx

--- components/tracking.jsx
+++ components/tracking.jsx
@@ -7,17 +7,13 @@ const Tracking = () => {
             />
             <img alt="fb" height="1" width="1" style={{ position: 'absolute' }}
                 src="https://www.facebook.com/tr?id=1650870088530325&ev=PageView&noscript=1" />
-
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-6H60V3S51H"></script>
             <script dangerouslySetInnerHTML={{
                 __html: `
-                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-                    ga('create', 'UA-55852483-1', 'auto');
-                    ga('send', 'pageview');
-                    `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', 'G-6H60V3S51H');
                 }}
             />
         </div>
