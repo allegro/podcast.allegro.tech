235cc3e9c3e98847aa516588eacb2214ff3fb6b5
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegro.pl>
2022-01-25 15:57:52 +0100

    change format date to pl-pl

--- src/lib/format-date.mjs
+++ src/lib/format-date.mjs
@@ -1,4 +1,4 @@
 export function formatDate(dateString) {
     const options = { year: "numeric", month: "long", day: "numeric" }
-    return new Date(dateString).toLocaleDateString(undefined, options)
+    return new Date(dateString).toLocaleDateString("pl-PL", options)
 }
