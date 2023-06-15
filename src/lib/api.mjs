91327c3145ec4899a7ea86d8351d5809803ba037
Bartosz Galek <bartosz.galek@allegro.pl>
2023-06-15 10:57:03 +0200

    migrated to spotify platform

--- src/lib/api.mjs
+++ src/lib/api.mjs
@@ -1,60 +1,43 @@
-import podcasts from '../_data/podcasts.json';
+import podcastsData from '../_data/podcasts.json';
 import authors from '../_data/authors.json';
-function displayEpisode(podcast){
-    return `S${(podcast.season + "").padStart(2, '0')}E${(podcast.episode + "").padStart(2, '0')}`
-  }
 
-function getPodcastImage(podcast){
-    return podcast.season > 2 ? "/img/podcast-player/podcast.png" : "/img/podcast-player/podcast-old.png"
-  }
+export const podcasts = podcastsData.map(entry => ({
+  ...entry,
+  hostData: {
+    ...authors[entry.host],
+    author: entry.host
+  },
+  authorsData: entry.authors.map((singleAuthor) => {
+    const authorData = authors[singleAuthor.author]
+    authorData.author = singleAuthor.author
+    return authorData
+  })
+})).sort((a, b) => b.date.localeCompare(a.date));
 
-function getPodcastCovers(authors){
-    return authors.map(({author, _}) => {
-            return "/img/authors/" + author + ".jpg"
-        }
-    )
-  }
-
-function getGuestNames(authors){
-    return authors.map(({_, name}) => {return name}).join(", ")
-  }
-
-
-function getData() {
-    return podcasts.map(podcast => {
-        let authorsData = podcast.authors.map((singleAuthor) => {
-            let authorData = authors[singleAuthor.author]
-            authorData.author = singleAuthor.author
-            return authorData
-        })
-        let hostData = authors[podcast.host];
-        hostData.author = podcast.host;
-        podcast.authorsData = authorsData;
-        podcast.hostData = hostData;
-        return Object.assign(podcast, {
-            podcastTitle : displayEpisode(podcast)  + " - " + getGuestNames(podcast.authors)   + " - " +  podcast.title,
-            audioSource : podcast.platforms.buzzsprout + ".mp3",
-            podcastPlatforms : podcast.platforms,
-            podcastCovers : getPodcastCovers(podcast.authors),
-            podcastImage : getPodcastImage(podcast)
-          })
-    }).sort((a, b) => b.date.localeCompare(a.date))
+function displayEpisode(podcast) {
+  return `S${(podcast.season + "").padStart(2, '0')}E${(podcast.episode + "").padStart(2, '0')}`
 }
 
-const AllPodcasts = getAllPodcasts();
+function getPodcastImage(podcast) {
+  return podcast.season > 2 ? "/img/podcast-player/podcast.png" : "/img/podcast-player/podcast-old.png"
+}
+
+function getPodcastCovers(authors) {
+  return authors.map(({ author, _ }) => {
+    return "/img/authors/" + author + ".jpg"
+  }
+  )
+}
+
+function getGuestNames(authors) {
+  return authors.map(({ _, name }) => { return name }).join(", ")
+}
+
 
 export function getPodcastBySlug(slug) {
-    return AllPodcasts.filter(item => item.slug === slug)[0]
+  return podcasts.filter(item => item.slug === slug)[0]
 }
 
 export function getAllSlugs() {
-    return AllPodcasts.map(item => item.slug)
+  return podcasts.map(item => item.slug)
 }
-
-
-export function getAllPodcasts() {
-    return getData();
-}
-
-
-
--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,10 +1,8 @@
 import rss from "@astrojs/rss";
-import { getAllPodcasts } from "../lib/api.mjs";
+import { podcasts } from "../lib/api.mjs";
 
-const podcastsEnriched = getAllPodcasts();
-
-export const get = () =>
-  rss({
+export function get(){
+  return rss({
     title: "allegro.tech podcast",
     description:
       "Misją Allegro Tech jest dzielenie się wiedzą oraz dobrymi praktykami. Podcasty obok tech bloga są naszym sposobem aby opowiadać o tym jakie wyznajemy wartości oraz w jakim kierunku zmierzamy. Kolejne odcinki będą zbiorem angażujących historii, dobrych praktyk i inspirujących case-studies. Do usłyszenia!.",
@@ -16,3 +14,4 @@ export const get = () =>
       pubDate: item.date,
     })),
   });
+}
