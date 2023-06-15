b46e20c3f7da0c578c1eb6c00e4eecb27e1d4bca
Bartosz Galek <bartosz.galek@allegro.pl>
2023-06-15 11:08:15 +0200

    fixing build

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -7,7 +7,7 @@ export function get(){
     description:
       "Misją Allegro Tech jest dzielenie się wiedzą oraz dobrymi praktykami. Podcasty obok tech bloga są naszym sposobem aby opowiadać o tym jakie wyznajemy wartości oraz w jakim kierunku zmierzamy. Kolejne odcinki będą zbiorem angażujących historii, dobrych praktyk i inspirujących case-studies. Do usłyszenia!.",
     site: import.meta.env.SITE,
-    items: podcastsEnriched.map((item) => ({
+    items: podcasts.map((item) => ({
       title: item.podcastTitle,
       description: item.toc.join(" "),
       link: "/" + item.slug,
