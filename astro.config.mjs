bfbd73b8b643f686bc505d1ecf59cc7a7cbcc487
Bartosz Galek <bartosz.galek@allegro.pl>
2023-06-15 11:07:22 +0200

    fixing build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://podcast.allegro.tech/',
-    integrations: [svelte(), sitemap()]
+    integrations: [sitemap()]
 });
